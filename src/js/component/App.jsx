e546a5f8d9582546a853459273d4cbb4e2020b6e
JHPBri <janghsuh@gmail.com>
2022-05-19 00:55:46 +0000

    useEffect

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //mock data
 import data from "./data.json";
 //components
@@ -8,6 +8,11 @@ import ToDoForm from "./ToDoForm.jsx";
 
 function App() {
 	const [toDoList, setToDoList] = useState(data);
+
+	useEffect(() => {
+		console.log("use effect ran");
+		console.log(toDoList);
+	}, [toDoList]);
 
 	const handleToggle = (id) => {
 		let mapped = toDoList.map((label) => {
@@ -30,28 +35,6 @@ function App() {
 		copy = [...copy, { label: userInput, done: false }];
 		setToDoList(copy);
 	};
-
-	fetch("https://assets.breatheco.de/apis/fake/todos/user/jhpbri", {
-		method: "PUT",
-		body: JSON.stringify(data),
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-		.then((resp) => {
-			console.log(resp.ok); // will be true if the response is successfull
-			console.log(resp.status); // the status code = 200 or code = 400 etc.
-			console.log(resp.text()); // will try return the exact result as string
-			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-		})
-		.then((data) => {
-			//here is were your code should start after the fetch finishes
-			console.log(data); //this will print on the console the exact object received from the server
-		})
-		.catch((error) => {
-			//error handling
-			console.log(error);
-		});
 
 	return (
 		<div className="App">
